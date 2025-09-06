bf353c63a30bce39efcbfbf3a715dc031f695806
willian_gmg <willian.gattass@sou.ufmt.br>
2025-09-06 12:29:14 -0400

    Adiciona componente DataNascimento e integra ao componente MaisFiltros

---
+++ project/src/components/Search/DataNascimento.jsx
@@ -0,0 +1,16 @@
+const DataNascimento = ({ value, onChange }) => (
+  <div>
+    <label className="text-sm font-medium text-gray-700 mb-1">
+      Data de Nascimento:
+    </label>
+    <input
+      type="date"
+      value={value}
+      onChange={onChange}
+      className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-colors"
+      placeholder="Selecione a data de nascimento"
+    />
+  </div>
+);
+
+export default DataNascimento;
--- project/src/components/Search/MaisFiltros.jsx
+++ project/src/components/Search/MaisFiltros.jsx
@@ -1,23 +1,24 @@
+import DataNascimento from "./DataNascimento";
 import InputLocalDesaparecimento from "./InputLocalDesaparecimento";
 import SelectSexo from "./SelectSexo";
 
 export const MaisFiltros = ({ searchParams, updateParam }) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 pt-4 border-t border-gray-200">
-      <div>
-        <SelectSexo
-          value={searchParams.sexo}
-          onChange={(e) => updateParam("sexo", e.target.value)}
-        />
-      </div>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 pt-4 border-t border-gray-200">
+      <SelectSexo
+        value={searchParams.sexo}
+        onChange={(e) => updateParam("sexo", e.target.value)}
+      />
 
-      <div>
-        <InputLocalDesaparecimento
-          value={searchParams.local_desaparecimento}
-          onChange={(e) => updateParam("local_desaparecimento", e.target.value)}
-        />
+      <InputLocalDesaparecimento
+        value={searchParams.local_desaparecimento}
+        onChange={(e) => updateParam("local_desaparecimento", e.target.value)}
+      />
 
-      </div>
+      <DataNascimento
+        value={searchParams.data_nascimento}
+        onChange={(e) => updateParam("data_nascimento", e.target.value)}
+      />
     </div>
   );
 };
