f3f24ddabf70e289ff521216c1d19fa791946a7b
willian_gmg <willian.gattass@sou.ufmt.br>
2025-09-07 20:04:44 -0400

    Corrige os valores das opções do seletor de sexo para uso de letras maiúsculas

--- project/src/components/Search/SelectSexo.jsx
+++ project/src/components/Search/SelectSexo.jsx
@@ -9,9 +9,9 @@ const SelectSexo = ({ value, onChange }) => (
       className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-colors"
     >
       <option value="">Selecione o sexo</option>
-      <option value="masculino">Masculino</option>
-      <option value="feminino">Feminino</option>
-      <option value="outro">Outro</option>
+      <option value="MASCULINO">Masculino</option>
+      <option value="FEMININO">Feminino</option>
+      <option value="OUTRO">Outro</option>
     </select>
   </div>
 );
